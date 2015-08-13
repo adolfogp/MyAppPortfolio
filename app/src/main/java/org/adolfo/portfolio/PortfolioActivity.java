fcec73d63df6fa016d6bedccce1fb54b3aa9fa2c
Jesús Adolfo García Pasquel <adolfogp@gmail.com>
2015-08-12 19:20:05 -0500

    Added listeners to the buttons.

--- app/src/main/java/org/adolfo/portfolio/PortfolioActivity.java
+++ app/src/main/java/org/adolfo/portfolio/PortfolioActivity.java
@@ -4,14 +4,71 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
+/**
+ * Activity that allows the user to start the apps developed during the
+ * <a href="https://www.udacity.com/course/android-developer-nanodegree--nd801">Android Developer
+ * Nanodegree</a>.
+ *
+ * @author Jesús Adolfo García Pasquel
+ */
 public class PortfolioActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portfolio);
+        setListeners();
+    }
+
+    /**
+     * Assings the {@link android.view.View.OnClickListener}s to the Activity's buttons.
+     */
+    private void setListeners() {
+        findViewById(R.id.button_spotify_app).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSpotifyStreamerApp();
+            }
+        });
+
+        findViewById(R.id.button_scores_app).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startScoresApp();
+            }
+        });
+
+        findViewById(R.id.button_library_app).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startLibraryApp();
+            }
+        });
+
+        findViewById(R.id.button_build_it_app).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startBuildItBiggerApp();
+            }
+        });
+
+        findViewById(R.id.button_xyz_reader_app).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startXyzReaderApp();
+            }
+        });
+
+        findViewById(R.id.button_capstone_app).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startCapstoneApp();
+            }
+        });
+
     }
 
     @Override
@@ -30,10 +87,59 @@ public class PortfolioActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_about) {
-            Toast.makeText(this, R.string.about_author, Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, R.string.toast_about_author, Toast.LENGTH_SHORT).show();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    /**
+     * Starts the Spotify Streamer app.
+     */
+    public void startSpotifyStreamerApp() {
+        // TODO - Start app and remove toast
+        Toast.makeText(this, R.string.toast_spotify_app, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Starts the Scores app.
+     */
+    public void startScoresApp() {
+        // TODO - Start app and remove toast
+        Toast.makeText(this, R.string.toast_scores_app, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Starts the Library app.
+     */
+    public void startLibraryApp() {
+        // TODO - Start app and remove toast
+        Toast.makeText(this, R.string.toast_library_app, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Starts the Build It Bigger app.
+     */
+    public void startBuildItBiggerApp() {
+        // TODO - Start app and remove toast
+        Toast.makeText(this, R.string.toast_build_it_bigger_app, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Starts the XYZ Reader app.
+     */
+    public void startXyzReaderApp() {
+        // TODO - Start app and remove toast
+        Toast.makeText(this, R.string.toast_xyz_reader_app, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Starts the Capstone app.
+     */
+    public void startCapstoneApp() {
+        // TODO - Start app and remove toast
+        Toast.makeText(this, R.string.toast_capstone_app, Toast.LENGTH_SHORT).show();
+    }
+
 }
